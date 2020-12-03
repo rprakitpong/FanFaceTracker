4afefce39eac57458975f9edbdae36184851d1c8
rprakitpong <rprakitpong@gmail.com>
2020-12-02 18:55:38 -0800

    track movement to face

--- Comp/main.py
+++ Comp/main.py
@@ -17,9 +17,10 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # set up serial
 serialPort = serial.Serial(port = "COM6", baudrate=9600, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+maxMovement = int(255 / 2)
 
 # set up parameters
-threshold = 0.2 # face has in center of image, with x percent threshold
+threshold = 0.05 # face has in center of image, with x percent threshold
 loBound = int(width / 2 - (width * threshold) / 2)
 hiBound = int(width / 2 + (width * threshold) / 2)
 
@@ -34,19 +35,37 @@ while(True):
 
     # recolor frame to highlight threshold
     ball = img[:, loBound:hiBound]
-    #ball = cv2.cvtColor(ball, cv2.COLOR_BGR2GRAY)
-    #ball = cv2.cvtColor(ball, cv2.COLOR_GRAY2BGR)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+    img = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
     img[:, loBound:hiBound] = ball
 
     if len(faces) > 0:
         (x,y,w,h) = faces[0] # only detect one face at a time
-        print(str(x) + ',' + str(y))
+        #print(str(x) + ',' + str(y))
 
-        # draw faces
+        # draw face on frame
         img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-                
+
+        # make x is center of face instead of left edge
+        x = int(x + w / 2)
+
+        # send new movement command if face is found outside bound
+        if x < loBound and serialPort.in_waiting == 0:
+            ratio = (loBound - x) / loBound             # farther from loBound, higher ratio
+            movement = int(ratio * maxMovement)
+            toSend = maxMovement - movement
+            toSend_byte = bytes([toSend])
+            serialPort.write(toSend_byte)
+        if x > hiBound and serialPort.in_waiting == 0:        
+            ratio = (x - hiBound) / (width - hiBound)   # farther from hiBound, higher ratio
+            movement = int(ratio * maxMovement)
+            toSend = maxMovement + movement
+            toSend_byte = bytes([toSend])
+            serialPort.write(toSend_byte)
+
+        # if movement is finished, reset input buffer so new commands can be sent
+        if serialPort.in_waiting > 0:
+            serialPort.reset_input_buffer()
+        
     # show frame
     cv2.imshow('frame', img)
 
